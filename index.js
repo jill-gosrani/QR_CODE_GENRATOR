1b28bce2e4cafa86512e14db9665783cb43f9b5a
jill-gosrani <jillgosrani15@gmail.com>
2024-03-24 21:58:36 -0400

    Added gitignore and changed UI

--- index.js
+++ index.js
@@ -3,28 +3,27 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
-import inquirer from 'inquirer';
+// import inquirer from 'inquirer';
 import qr from "qr-Image";
-import fs from 'fs';
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {message:"Type/Paste your link here: ", name:"URL"},
-  ])
-  .then((answers) => {
+import express from "express";
+import bodyParser from "body-parser";
 
-    var qr_svg = qr.image(answers.URL);
-    qr_svg.pipe(fs.createWriteStream("qrImg.png"));
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/", (req, res)=>{
+  res.render("index.ejs");
+});
 
-    fs.writeFile("QRCode.txt", answers.URL, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-  })
-})
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+app.post("/submit", (req, res)=>{
+  const qrLink = req.body["links"];
+  var qr_svg = qr.imageSync(qrLink);
+  var qrBase64 = qr_svg.toString('base64');
+  res.render("index.ejs", {
+    img: qrBase64
   });
+});
+
+app.listen(PORT,()=>{
+  console.log(`Listning to port ${PORT}`);
+});
